d95081b0b6e42b0a9f891a449f92787ba692c7d7
Thief3 <kissarlim@gmail.com>
2021-02-10 13:22:49 +0000

    Removed Home from home page title

--- microblog/app/helpers/application_helper.rb
+++ microblog/app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # Returns full title
-  def full_title(page_title = "")
+  def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
-    if base_title.empty?
+    if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
