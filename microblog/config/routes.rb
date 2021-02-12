cf7cfc70404ce251fa29a4747851eb3cbf3131d1
Thief3 <kissarlim@gmail.com>
2021-02-12 17:33:50 +0000

    Added session controller and login page basics.

--- microblog/config/routes.rb
+++ microblog/config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-
   resources :users
+
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
