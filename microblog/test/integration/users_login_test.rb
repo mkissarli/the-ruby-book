f1f55eaae3b5b9468ee816a30bdd2b6fa9bd9cc0
Thief3 <kissarlim@gmail.com>
2021-02-12 20:01:36 +0000

    Added logout functionlity and tests.

--- microblog/test/integration/users_login_test.rb
+++ microblog/test/integration/users_login_test.rb
@@ -33,10 +33,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template "sessions/new"
     post login_path, params: { session: { email: @user.email,
                                           password: ""}}
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "logout after valid login" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: "password"}}
+
     assert is_logged_in?
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count:0
+    assert_select "a[href=?]", user_path(@user), count:0
   end
 end
