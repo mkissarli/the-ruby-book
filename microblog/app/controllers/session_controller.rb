ade5f818811d2825800f16f169a0fbd0c17242a6
Thief3 <kissarlim@gmail.com>
2021-02-12 17:45:53 +0000

    In the process of creating a login. Fixed flash display error.

--- microblog/app/controllers/session_controller.rb
+++ microblog/app/controllers/session_controller.rb
@@ -1,4 +1,17 @@
 class SessionController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #redirect_to 
+    else
+      flash[:danger] = 'Invalid email/password combination.'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
