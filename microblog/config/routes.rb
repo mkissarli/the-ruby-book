522113d01815561f523772f6f1f30b227ddcbf87
Thief3 <kissarlim@gmail.com>
2021-02-12 17:56:23 +0000

    Refactor

--- microblog/config/routes.rb
+++ microblog/config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users
 
-  get '/login', to: 'session#new'
-  post '/login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
