08be77f44beba46ae659adaa55dbf9d5e21a5c06
Thief3 <kissarlim@gmail.com>
2021-02-13 21:45:48 +0000

    Polished edits page.

---
+++ microblog/test/integration/users_edit_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "Unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "foot@invalid",
+                                              password: "11",
+                                              password_confirmation: "12" }}
+    assert_template 'users/edit'
+    assert_select 'div.alert', 'The form contains 4 errors.'
+  end
+
+  test "Successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+end
