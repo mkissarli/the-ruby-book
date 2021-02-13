ac5c19ec1ff40b19091b8507cd1271e1de13694f
Thief3 <kissarlim@gmail.com>
2021-02-13 21:28:06 +0000

    Added update method for user controller.

--- microblog/app/controllers/users_controller.rb
+++ microblog/app/controllers/users_controller.rb
@@ -21,6 +21,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # pass
+    else
+      render 'edit'
+    end
+  end
   
   private
 
