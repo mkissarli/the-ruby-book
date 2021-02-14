b26567b6e93387fbdeb06532443c474dce1c2fd1
Thief3 <kissarlim@gmail.com>
2021-02-14 14:47:29 +0000

    Tests and refactor.

--- microblog/test/integration/users_signup_test.rb
+++ microblog/test/integration/users_signup_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "invalid signup information" do
+    get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: {
              user: {
@@ -13,10 +19,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
            }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
-  test "valid signup" do
-    assert_difference "User.count" do
+  test "valid signup information with account activation" do
+    get signup_path
+    assert_difference "User.count", 1 do
       post users_path, params: {
              user: {
                name: "Mike",
@@ -26,9 +35,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
              }
            }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
 
+    # Try to log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    assert_template root_url
+
+    # Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    assert_template root_url
+
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: "no")
+    assert_not is logged_in?
+
+    # Correct
+    get edit_account_activation_path(iser.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    #assert_template 'users/show'
-    #assert_not flash.blank?
+    assert_template 'users/show'
+    assert_not flash.blank?
   end
 end
