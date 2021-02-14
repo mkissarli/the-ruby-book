7e8d10ea2b801d91bcf1acdb1fa9c9f8ff653531
Thief3 <kissarlim@gmail.com>
2021-02-14 13:47:41 +0000

    Added account activation fields and fixtures.

--- microblog/app/models/user.rb
+++ microblog/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   
   before_save { email.downcase! }
   
@@ -40,5 +42,16 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(self.activation_token)
   end
 end
