2e346963cf4d3aedb8a754375f99b78af8a5974e
Thief3 <kissarlim@gmail.com>
2021-02-15 10:54:43 +0000

    Added image tests.

--- microblog/test/integration/microposts_interface_test.rb
+++ microblog/test/integration/microposts_interface_test.rb
@@ -9,7 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -19,9 +19,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = "This micropost really ties the room together."
+    image = fixture_file_upload("test/fixtures/coffee.png", 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, image: image } }
     end
+    assert @user.microposts.first.reload.image.attached?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
