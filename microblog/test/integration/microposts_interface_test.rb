c09cd0ab12b4df6d9701437ce47f87ff0fc35567
Thief3 <kissarlim@gmail.com>
2021-02-15 10:42:38 +0000

    Added integration test.

--- microblog/test/integration/microposts_interface_test.rb
+++ microblog/test/integration/microposts_interface_test.rb
@@ -37,4 +37,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+
+    # User with zero microposts
+    other_user = users(:pam)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "sploosh")
+    get root_path
+    assert_match "sploosh", response.body
+    assert_match "1 micropost", response.body
+  end
 end
