05dcfeb75dff115c6303474bc73625f7a2e1f70b
Thief3 <kissarlim@gmail.com>
2021-02-15 08:23:26 +0000

    Added microposts, connections, fixtures and some tests.

--- microblog/app/models/user.rb
+++ microblog/app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  has_many :microposts
+  
   # Returns the hash digest of a given string, used for passwords.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
